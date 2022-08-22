9dce363add3b32810bdbbd11096e4aa9779bf56d
antonkoshevoi <anton.koshevoi.dev@gmail.com>
2022-08-22 19:52:48 +0300

    history saving implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
 'use strict'
 
+const containerForHistory = document.createElement('div')
 const body = document.getElementById('body')
+const ulForList = document.createElement('ul')
 const input = document.createElement('input')
-const ul = document.createElement('ul')
 
 body.appendChild(input)
-body.appendChild(ul)
+body.appendChild(ulForList)
+body.appendChild(containerForHistory)
+
+const history = []
 
 ;(async () => {
   const obj = await fetch('https://jsonplaceholder.typicode.com/albums')
@@ -18,15 +22,27 @@ body.appendChild(ul)
   }
 
   posts.forEach(post => {
-    let li = document.createElement('li')
-    li.innerHTML = post
-    ul.appendChild(li)
+    const liForList = document.createElement('li')
+    liForList.innerHTML = post
+    ulForList.appendChild(liForList)
   })
 })()
 
-input.addEventListener('change', e => {
+input.addEventListener('change', ({ target: { value } }) => {
   const list = document.querySelectorAll('li')
 
   for (let i = 0; i < list.length; i++) {
-    list[i].textContent.toLowerCase().indexOf(e.target.value.toLowerCase()) === 0 ? (list[i].style.display = 'block') : (list[i].style.display = 'none')}
+    list[i].textContent.toLowerCase().indexOf(value.toLowerCase()) === 0 ? (list[i].style.display = 'block') : (list[i].style.display = 'none')
+  }
+
+  history.push({ id: Date.now(), value })
+
+  if (value.length) {
+    const itemHistory = document.createElement('div')
+    const buttonForDelete = document.createElement('button')
+    buttonForDelete.innerHTML = 'X'
+    itemHistory.innerHTML = `${value} ${new Date().toLocaleString('en')}`
+    itemHistory.appendChild(buttonForDelete)
+    containerForHistory.appendChild(itemHistory)
+  }
 })
