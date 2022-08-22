770a878309a9d24ca426062baf01a6921113a4fb
antonkoshevoi <anton.koshevoi.dev@gmail.com>
2022-08-22 20:47:13 +0300

    implemented deleting items from the history

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,19 +30,24 @@ const history = []
 
 input.addEventListener('change', ({ target: { value } }) => {
   const list = document.querySelectorAll('li')
+  const id = Date.now()
 
   for (let i = 0; i < list.length; i++) {
     list[i].textContent.toLowerCase().indexOf(value.toLowerCase()) === 0 ? (list[i].style.display = 'block') : (list[i].style.display = 'none')
   }
 
-  history.push({ id: Date.now(), value })
+  history.push({ id, value })
 
   if (value.length) {
     const itemHistory = document.createElement('div')
     const buttonForDelete = document.createElement('button')
+    itemHistory.id = id
+    buttonForDelete.id = id
     buttonForDelete.innerHTML = 'X'
     itemHistory.innerHTML = `${value} ${new Date().toLocaleString('en')}`
     itemHistory.appendChild(buttonForDelete)
     containerForHistory.appendChild(itemHistory)
   }
+
+  document.querySelectorAll('button').forEach(button => button.addEventListener('click', ({ target: { id } }) => document.getElementById(id).remove()))
 })
