f36188344bb380e562b8a438d1e1398f1778fa5e
antonkoshevoi <anton.koshevoi.dev@gmail.com>
2022-08-22 18:59:51 +0300

    added quick search

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 'use strict'
 
 const body = document.getElementById('body')
+const input = document.createElement('input')
 const ul = document.createElement('ul')
 
+body.appendChild(input)
 body.appendChild(ul)
 
-let posts = []
+;(async () => {
+  const obj = await fetch('https://jsonplaceholder.typicode.com/albums')
+  const data = await obj.json()
 
-fetch('https://jsonplaceholder.typicode.com/albums')
-  .then(resp => resp.json())
-  .then(data => {
-    posts = data
-    return posts.map(post => {
-      let li = document.createElement('li')
-      li.innerHTML = post.title
-      ul.appendChild(li)
-    })
+  const posts = []
+
+  for (let i = 0; i < data.length; i++) {
+    posts.push(data[i].title)
+  }
+
+  posts.forEach(post => {
+    let li = document.createElement('li')
+    li.innerHTML = post
+    ul.appendChild(li)
   })
-  .catch(error => console.error(error))
+})()
+
+input.addEventListener('change', e => {
+  const list = document.querySelectorAll('li')
+
+  for (let i = 0; i < list.length; i++) {
+    list[i].textContent.toLowerCase().indexOf(e.target.value.toLowerCase()) === 0 ? (list[i].style.display = 'block') : (list[i].style.display = 'none')}
+})
